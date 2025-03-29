827d133213989e012d2099dcfdff97d10ef2f0f8
KhussThakrani <157315731+KhussThakrani@users.noreply.github.com>
2025-03-28 22:26:03 -0300

    Updated game_board.h
    Add board structure and setup functions
    
    - Defined Cell struct and board dimensions
    - Declared functions to initialize and print the board

--- game_board.h
+++ game_board.h
@@ -1,1 +1,16 @@
+#ifndef GAME_BOARD_H
+#define GAME_BOARD_H
 
+#define HEIGHT 27
+#define WIDTH 43
+
+typedef struct Cell {
+    char type;  // 'S' for snake, 'F' for food, ' ' for empty
+} Cell;
+
+// Function declarations
+void initializeBoard(Cell gameBoard[HEIGHT][WIDTH]);
+void printBoard(Cell gameBoard[HEIGHT][WIDTH]);
+void clearBoard(Cell gameBoard[HEIGHT][WIDTH]);
+
+#endif // GAME_BOARD_H 

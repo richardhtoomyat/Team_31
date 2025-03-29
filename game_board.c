f3965673e783da4b3da3c842bd3f79c08b189b8a
KhussThakrani <157315731+KhussThakrani@users.noreply.github.com>
2025-03-28 22:23:43 -0300

    Updated game_board.c
    Set up basic game board display
    
    - Added logic to initialize the game board with borders and empty space
    - Printed the board to the terminal using simple characters like |, -, and +
    - This gives us a clean visual frame for the Snake game

--- game_board.c
+++ game_board.c
@@ -1,1 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "game_board.h"
+
+void initializeBoard(Cell gameBoard[HEIGHT][WIDTH]) {
+    
+    for (int i = 0; i < HEIGHT; i++) {
+        for (int j = 0; j < WIDTH; j++) {
+            if (i == 0 || i == HEIGHT-1) {
+                gameBoard[i][j].type = (j == 0 || j == WIDTH-1) ? '+' : '-';
+            } else if (j == 0 || j == WIDTH-1) {
+                gameBoard[i][j].type = '|';
+            } else {
+                gameBoard[i][j].type = ' ';
+            }
+        }
+    }
+}
+
+void printBoard(Cell gameBoard[HEIGHT][WIDTH]) {
+     
+    for (int i = 0; i < HEIGHT; i++) {
+        for (int j = 0; j < WIDTH; j++) {
+            printf("%c", gameBoard[i][j].type);
+        }
+        printf("\n");
+    }
+}
 

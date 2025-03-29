5aeff131fb4fbd9642430e2ac70afd345cb6be7d
richardhtoomyat <39412918+richardhtoomyat@users.noreply.github.com>
2025-03-29 17:25:10 -0300

    Update snake.h
    added snake header file

--- snake.h
+++ snake.h
@@ -1,1 +1,27 @@
+#ifndef SNAKE_H
+#define SNAKE_H
 
+#include "game_board.h"
+
+// Snake segment structure (linked list node)
+typedef struct SnakeSegment {
+    int x, y;
+    struct SnakeSegment* next;
+} SnakeSegment;
+
+// Snake structure with head pointer and direction
+typedef struct Snake {
+    SnakeSegment* head;
+    char direction;  // 'W', 'A', 'S', 'D'
+    int length;     // Current length of snake
+} Snake;
+
+// Function declarations
+void initializeSnake(Snake* snake);
+void moveSnake(Snake* snake, char input);
+void addSegment(Snake* snake);
+void updateBoardWithSnake(Cell gameBoard[HEIGHT][WIDTH], Snake* snake);
+int checkCollisions(Snake* snake);
+
+
+#endif // SNAKE_H 

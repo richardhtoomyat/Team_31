602935152e166d983e90e44ebb5abf71abdf4b16
Tejas Padaliya <tejasvijaybhai.padaliya@unb.ca>
2025-03-28 22:02:39 -0300

    Update food.h

--- food.h
+++ food.h
@@ -1,1 +1,11 @@
+#ifndef FOOD_H
+#define FOOD_H
 
+#include "game_board.h"
+#include "snake.h"
+
+void generateFood(Cell gameBoard[HEIGHT][WIDTH], Snake* snake);
+int isFood(Cell gameBoard[HEIGHT][WIDTH], int x, int y);
+int isValidFoodPosition(Cell gameBoard[HEIGHT][WIDTH], Snake* snake, int x, int y);
+
+#endif // FOOD_H 

c0a2436ea5ef2400024c2fc12215953801e86e77
Tejas Padaliya <tejasvijaybhai.padaliya@unb.ca>
2025-03-28 22:03:52 -0300

    Update movement.h

--- movement.h
+++ movement.h
@@ -1,1 +1,9 @@
+#ifndef MOVEMENT_H
+#define MOVEMENT_H
 
+void setupInput(void);
+void restoreTerminalSetting(void);
+char getInput(void);
+void changeTerminalSetting(void);
+
+#endif // MOVEMENT_H 

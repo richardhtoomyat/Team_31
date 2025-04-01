50f0d34bfa6464007809cdfbe443c6a764b0b3ca
KhussThakrani <157315731+KhussThakrani@users.noreply.github.com>
2025-03-31 21:34:00 -0300

    Updated with descriptive comments game_board.c

--- game_board.c
+++ game_board.c
@@ -3,19 +3,32 @@
 #include "game_board.h"
 
 void initializeBoard(Cell gameBoard[HEIGHT][WIDTH]) {
-    
-    for (int i = 0; i < HEIGHT; i++) {
-        for (int j = 0; j < WIDTH; j++) {
-            if (i == 0 || i == HEIGHT-1) {
-                gameBoard[i][j].type = (j == 0 || j == WIDTH-1) ? '+' : '-';
-            } else if (j == 0 || j == WIDTH-1) {
-                gameBoard[i][j].type = '|';
-            } else {
-                gameBoard[i][j].type = ' ';
+    for (int row = 0; row < HEIGHT; row++) {
+        for (int col = 0; col < WIDTH; col++) {
+            
+            // Looks through corners and sets +
+            if ((row == 0 || row == HEIGHT - 1) && (col == 0 || col == WIDTH - 1)) {
+                gameBoard[row][col].type = '+';
+            }
+                
+            // Looks through top and bottom edges and sets -
+            else if (row == 0 || row == HEIGHT - 1) {
+                gameBoard[row][col].type = '-';
+            }
+                
+            // Looks through left and right edges and sets |
+            else if (col == 0 || col == WIDTH - 1) {
+                gameBoard[row][col].type = '|';
+            }
+
+            // if none then gives a empty space
+            else {
+                gameBoard[row][col].type = ' ';
             }
         }
     }
 }
+
 
 void printBoard(Cell gameBoard[HEIGHT][WIDTH]) {
      
